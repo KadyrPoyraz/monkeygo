fb612bd455fccce360d86e2a20398e0827e2a984
KadyrPoyraz <kadyrpoyraz@gmail.com>
2024-03-15 21:14:10 +0600

    Started work on evaluating

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -39,6 +39,10 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.IfExpression:
 		return evalIfExpression(node)
+
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 	}
 
 	return nil
@@ -152,6 +156,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
 	for _, statement := range stmts {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 
 	return result
--- object/object.go
+++ object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-    BOOLEAN_OBJ = "BOOLEAN"
-    NULL_OBJ = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -51,3 +52,14 @@ func (i *Null) Type() ObjectType {
 	return NULL_OBJ
 }
 
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType {
+    return RETURN_VALUE_OBJ 
+}
+
+func (rv *ReturnValue) Inspect() string  {
+    return rv.Value.Inspect() 
+}
